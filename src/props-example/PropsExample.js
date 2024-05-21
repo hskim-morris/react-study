ddc89d77682b934a59afc9201d5fdc4043f15996
Morris <hskim@stcomm.kr>
2024-05-21 12:55:22 +0900

    20240521

--- src/props-example/PropsExample.js
+++ src/props-example/PropsExample.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useState } from "react";
 
 function Header(props) {
     console.log('props', props, props.title);
     return (<header>
         <h1>
-            <a href="/">{props.title}</a>
+            <a href="/" onClick={event=> {
+                    event.preventDefault();
+                    props.onChangeMode();
+                }}>{props.title}</a>
         </h1>
     </header>);
 }
@@ -15,7 +19,10 @@ function Nav(props) {
         const element = props.topics[i];
         list.push(
             <li key={element.id}>
-                <a href={'/read' + element.id}>{element.title}</a>
+                <a id={element.id} href={'/read/' + element.id} onClick={event=> {
+                    event.preventDefault();
+                    props.onChangeMode(Number(event.target.id));
+                }}>{element.title}</a>
             </li>
         );
     }
@@ -35,17 +42,44 @@ function Aticle(props) {
 }
 
 function PropsExample() {
+
+    const [mode, setMode] = useState('WELCOME');
+    const [id, setId] = useState(null);
+    console.log('mode', mode);
+    console.log('id', id);
+
     const topics = [
-        {id:1, title: 'react-04-01', body: 'html ...'},
-        {id:2, title: 'react-04-02', body: 'css ...'},
-        {id:3, title: 'react-04-03', body: 'js ...'},
+        { id: 1, title: 'react-04-01', body: 'html ...' },
+        { id: 2, title: 'react-04-02', body: 'css ...' },
+        { id: 3, title: 'react-04-03', body: 'js ...' },
     ];
+
+    let content = null;
+
+    if (mode === 'WELCOME') {
+        content = <Aticle title="제목 테스트" body="바디 테스트" />
+    } else if (mode === 'READ') {
+        let title, body = null;
+        for (let i = 0; i < topics.length; i++) {
+            if( topics[i].id === id ) {
+                title = topics[i].title;
+                body = topics[i].body;
+            }
+            
+        }
+        content = <Aticle title={title} body={body} />
+    }
 
     return (
         <>
-            <Header title="TEST 할까"/>
-            <Nav topics={topics}/>
-            <Aticle title="제목 테스트" body="바디 테스트"/>
+            <Header title="TEST 할까" onChangeMode={() => { 
+                setMode('WELCOME');
+            }} />
+            <Nav topics={topics} onChangeMode={(_id) => {
+                setMode('READ');
+                setId(_id);
+            }} />
+            {content}
         </>
     );
 }
